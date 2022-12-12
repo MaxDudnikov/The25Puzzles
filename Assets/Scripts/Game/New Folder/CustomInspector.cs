87eb20caa41ec86b217919c26365cf93918aa9ea
Max <maksim_dudnikov_1999@mail.ru>
2022-12-12 16:06:59 +0300

    Новая версия

--- Assets/Scripts/Game/New Folder/CustomInspector.cs
+++ Assets/Scripts/Game/New Folder/CustomInspector.cs
@@ -1,6 +1,7 @@
 using UnityEditor;
 using UnityEngine;
 
+#if UNITY_EDITOR
 [CustomEditor(typeof(DrawGrid))]
 public class CustomInspector : Editor
 {
@@ -13,3 +14,4 @@ public class CustomInspector : Editor
             drawGrid.Draw();
     }
 }
+#endif
